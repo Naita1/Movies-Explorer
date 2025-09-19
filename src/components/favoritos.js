15803ad0d049580a0eb78702bec80b8e99a5f192
Taina_Ribeiro <tdelima25@gmail.com>
2025-09-19 14:09:52 -0300

    Add files via upload

---
+++ src/components/favoritos.js
@@ -0,0 +1,23 @@
+
+import "../style/home.css"
+
+export default function Home(){
+
+    return(
+        
+        <div>
+          <header>
+              <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="home.js"> Inicio </a>
+            </nav>
+              <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="favoritos.js"> Minha Lista </a>
+            </nav>
+
+         
+          </header>
+          <h1>aa</h1>
+        </div>
+    )
+}
+
--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react"
 import { fetchMovies } from "../services/api"
+import "../style/home.css"
+
 
 export default function Home(){
 
     const [movies, setMovies] = useState([])
-    const [loading, setLoading] = useState([true])
+    const [loading, setLoading] = useState(true)
 
     useEffect(()=>{
         async function loadMovies() {
@@ -21,14 +23,29 @@ export default function Home(){
         loadMovies()
     }, []);
 
-    if (loading) return <p>Carregando filmes...</p>
+    if (loading) return <p style={{color:"white"}}>Carregando filmes...</p>
   
     return(
+        
         <div>
-            <h1>Filmes</h1>
-            <div >{movies.map((movie) => (
-                <div key={movie.id}>
+          <header>
+              
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="/"> Inicio </a>
+            </nav>
+              <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="favoritos"> Minha Lista </a>
+            </nav>
+          </header>
+                        <h1>Filmes</h1>
+            <div className="movie_card">{movies.map((movie) => (
+                <div key={movie.id} className="movie_card_individual">
+
+                    <h3>{movie.original_title}</h3>
+                    <img className="movie_image" src={movie.image} alt={movie.title} width={100}></img>
                     <h3>{movie.title}</h3>
+                    <h3>{movie.rt_score}</h3>
+
                 </div>
             ))}</div>
         </div>
